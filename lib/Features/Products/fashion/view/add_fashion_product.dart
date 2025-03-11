b9eab659e1d5744bf0a61410ee4f20834fd2d160
musafarahamed <musafar.innerix@gmail.com>
2025-03-11 14:55:06 +0530

    folder added

---
+++ lib/Features/Products/fashion/view/add_fashion_product.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddFashionProduct extends StatefulWidget {
+  const AddFashionProduct({super.key});
+
+  @override
+  State<AddFashionProduct> createState() => _AddFashionProductState();
+}
+
+class _AddFashionProductState extends State<AddFashionProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
