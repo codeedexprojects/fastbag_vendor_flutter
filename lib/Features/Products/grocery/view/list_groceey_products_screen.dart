abefc8b639950e56c4398aba9e578215950dea5e
musafarahamed <musafar.innerix@gmail.com>
2025-03-20 09:35:53 +0530

    issue fixed

--- lib/Features/Products/grocery/view/list_groceey_products_screen.dart
+++ lib/Features/Products/grocery/view/list_groceey_products_screen.dart
@@ -1,4 +1,5 @@
 import 'package:fastbag_vendor_flutter/Commons/fb_button.dart';
+import 'package:fastbag_vendor_flutter/Commons/fonts.dart';
 import 'package:fastbag_vendor_flutter/Commons/text_field_decortion.dart';
 import 'package:fastbag_vendor_flutter/Extentions/navigation_helper.dart';
 import 'package:fastbag_vendor_flutter/Features/Products/View/product_detail_screen.dart';
@@ -6,6 +7,7 @@ import 'package:fastbag_vendor_flutter/Features/Products/grocery/ViewModel/groce
 import 'package:fastbag_vendor_flutter/Features/Products/grocery/model/grocery_catgeory_model.dart';
 import 'package:fastbag_vendor_flutter/Features/Products/grocery/model/grocery_sub_category_model.dart';
 import 'package:fastbag_vendor_flutter/Features/Products/grocery/view/add_grocery_product.dart';
+import 'package:fastbag_vendor_flutter/Features/Products/grocery/view/grocery_product_edit_delete_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -14,8 +16,10 @@ import 'package:provider/provider.dart';
 class ListGroceryProducts extends StatefulWidget {
   final GrocerySubCategoryModel subCategory;
 
-  const ListGroceryProducts(
-      {super.key, required this.subCategory, });
+  const ListGroceryProducts({
+    super.key,
+    required this.subCategory,
+  });
 
   @override
   State<ListGroceryProducts> createState() => _ListGroceryProductsState();
@@ -26,7 +30,7 @@ class _ListGroceryProductsState extends State<ListGroceryProducts> {
   void initState() {
     super.initState();
     final groceryViewModel =
-        Provider.of<GroceryViewModel>(context, listen: false);
+    Provider.of<GroceryViewModel>(context, listen: false);
     groceryViewModel.fetchProductList(context, widget.subCategory.id);
   }
 
@@ -63,116 +67,120 @@ class _ListGroceryProductsState extends State<ListGroceryProducts> {
               final products = groceryViewModel.subCategoryProducts;
               return products.isEmpty
                   ? Expanded(
-                      child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SvgPicture.asset(
-                            'assets/icons/no_product.svg',
-                            width: screenWidth * .45, // Set desired width
-                            height: screenWidth * .3, // Set desired height
-                          ),
-                          SizedBox(
-                            height: screenHeight * .004,
-                          ),
-                          const Text("Nothing to show yet. Created"),
-                          const Text("Product list will appear here")
-                        ],
-                      ),
-                    ))
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icons/no_product.svg',
+                          width: screenWidth * .45, // Set desired width
+                          height: screenWidth * .3, // Set desired height
+                        ),
+                        SizedBox(
+                          height: screenHeight * .004,
+                        ),
+                        Text("Nothing to show yet. Created", style: nunito()),
+                        Text("Product list will appear here", style: nunito())
+                      ],
+                    ),
+                  ))
                   : Expanded(
-                      child: ListView.builder(
-                        itemCount: products.length,
-                        itemBuilder: (context, index) {
-                          print(
-                              '-----------------> lenth ------------> ${products.length}');
-                          print(
-                              '-----------------> products ------------> ${products[index].name}');
-                          return Column(
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  navigate(
-                                      context: context,
-                                      screen: ProductDetailScreen(
-                                          productId: products[index].id));
-                                },
-                                child: ListTile(
-                                  leading: Container(
-                                    height: screenHeight * .05,
-                                    width: screenHeight * .06,
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                        image: products[index].images.isNotEmpty
-                                            ? NetworkImage(
-                                                products[index].images[0].image)
-                                            : const AssetImage(
-                                                'assets/Images/grocery.jpeg'),
-                                        fit: BoxFit.cover,
-                                      ),
-                                      border: Border.all(
-                                          color: Colors.grey, width: 0.2),
-                                    ),
-                                  ),
-                                  title: Text(products[index].name),
-                                  subtitle:
-                                      Text(products[index].price.toString()),
-                                  trailing: Switch(
-                                    value: false,
-                                    activeColor: Colors.green,
-                                    inactiveThumbColor: Colors.white,
-                                    inactiveTrackColor: Colors.grey[300],
-                                    trackOutlineColor:
-                                        const WidgetStatePropertyAll(
-                                            Colors.transparent),
-                                    materialTapTargetSize:
-                                        MaterialTapTargetSize.shrinkWrap,
-                                    onChanged: (value) {
-                                      // Handle switch toggle logic
-                                    },
-                                  ),
+                child: ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    print(
+                        '-----------------> lenth ------------> ${products.length}');
+                    print(
+                        '-----------------> products ------------> ${products[index].name}');
+                    return Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            navigate(
+                                context: context,
+                                screen: ProductDetailScreen(
+                                    productId: products[index].id));
+                          },
+                          child: ListTile(
+                            leading: Container(
+                              height: screenHeight * .05,
+                              width: screenHeight * .06,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: products[index].images.isNotEmpty
+                                      ? NetworkImage(
+                                      products[index].images[0].image)
+                                      : const AssetImage(
+                                      'assets/Images/grocery.jpeg'),
+                                  fit: BoxFit.cover,
                                 ),
+                                border: Border.all(
+                                    color: Colors.grey, width: 0.2),
                               ),
-                            ],
-                          );
-                        },
-                      ),
+                            ),
+                            title: Text(products[index].name),
+                            subtitle:
+                            Text(products[index].price.toString()),
+                            trailing: Switch(
+                              value: products[index].available,
+                              activeColor: Colors.green,
+                              inactiveThumbColor: Colors.white,
+                              inactiveTrackColor: Colors.grey[300],
+                              trackOutlineColor:
+                              const WidgetStatePropertyAll(
+                                  Colors.transparent),
+                              materialTapTargetSize:
+                              MaterialTapTargetSize.shrinkWrap,
+                              onChanged: (value) {
+                                groceryViewModel.updateProductAvailable(
+                                    context,
+                                    products[index].id,
+                                    !products[index].available);
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
                     );
+                  },
+                ),
+              );
             }),
             Padding(
               padding: EdgeInsets.symmetric(
                   horizontal: screenWidth / 15, vertical: 5),
               child: FbButton(
                   onClick: () {
+                    print('----------------------->${widget.subCategory.name}');
+
                     navigate(
                         context: context,
                         screen: AddGroceryProduct(
-                          
                           subCategory: widget.subCategory,
                         ));
                   },
                   label: "+ Add Product"),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: screenWidth / 15, vertical: 5),
-              child: FbButton(
-                onClick: () {
-                  // navigate(
-                  //     context: context,
-                  //     screen: ProductEditDeleteScreen(
-                  //         products: groceryViewModel.subCategoryProducts));
-                },
-                label: "Export",
-                icon: const FaIcon(
-                  FontAwesomeIcons.arrowUpFromBracket,
-                  size: 17,
+            if (groceryViewModel.subCategoryProducts.isNotEmpty)
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: screenWidth / 15, vertical: 5),
+                child: FbButton(
+                  onClick: () {
+                    navigate(
+                        context: context,
+                        screen: const GroceryProductEditDeleteScreen());
+                  },
+                  label: "Edit",
+                  icon: const FaIcon(
+                    FontAwesomeIcons.penToSquare,
+                    size: 20,
+                  ),
+                  color: Colors.white,
+                  textColor: Colors.blue,
+                  borderColor: Colors.blue,
                 ),
-                color: Colors.white,
-                textColor: Colors.green,
-                borderColor: Colors.green,
               ),
-            ),
             gap(.02),
           ],
         ),
